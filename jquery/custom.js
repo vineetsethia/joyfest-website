f5f17d2baaaf9ba8653aca0f96c0d113d2a126f3
vishwashrisairam <vishwashrisairam@gmail.com>
2015-07-10 20:51:50 +0530

    Update custom.js

--- jquery/custom.js
+++ jquery/custom.js
@@ -1,3 +1,12 @@
 $(document).ready(function(e){ 
-	 
+	$('.move').click(function(){
+		$('#content').css("z-index","1");
+
+		
+	});
+	$('#one').hover(function(){
+		$('#content').css("z-index","-100");
+
+		
+	});	 
 })
