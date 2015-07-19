800df7f9450cff04c8a4a353d67dcb7db97d8233
Vineet <u13co087@coed.svnit.ac.in>
2015-07-19 22:35:29 +0530

    Commited new changes & Updated Homepage

--- jquery/custom.js
+++ jquery/custom.js
@@ -1,3 +1,22 @@
 $(document).ready(function(e){ 
-	$('nav').hide();	 
+	$('.move').click(function(){
+		$('#content').css("z-index","1");
+
+		
+	});
+	$('#one').hover(function(){
+		$('#content').css("z-index","-100");
+
+		
+	});	 
+	$("#slideshow > div:gt(0)").hide();
+
+	setInterval(function() { 
+ 	 $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
 })
